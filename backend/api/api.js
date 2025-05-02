b730afe5ad1094e3b9b5fffc65ae8d5d71337176
nathazz <nathansg907@gmail.com>
2025-05-02 12:53:06 -0300

    style(form, cards): update styles and apply lint fixes

--- backend/api/api.js
+++ backend/api/api.js
@@ -1,26 +1,28 @@
-import express from 'express'
-import {scan} from './scanPage'
-import cors from 'cors'
+import express from "express";
+import { scan } from "./scanPage";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	}),
+);
 
-app.get('/api/:valor', async (req,res) =>{
-    try{
-        const value = req.params.valor;
-        const resultado = await scan(value);
-        res.json(resultado)
-        console.log(resultado)
-    } catch(error){
-        console.log(error)
-    }
-})
+app.get("/api/:valor", async (req, res) => {
+	try {
+		const value = req.params.valor;
+		const resultado = await scan(value);
+		res.status(200).json(resultado);
+	} catch (error) {
+		console.error("Error:", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
 
 app.listen(3030, () => {
-    console.log('Servidor UP')
-})
+	console.log("Server rodando em: http://localhost:3030");
+});
